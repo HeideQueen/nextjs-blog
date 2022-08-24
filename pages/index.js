138e7d9a21bd5a058c6a09096dbfe1fb5c8d9f42
HeideQueen <lucyfabz@gmail.com>
2022-08-24 00:58:57 -0500

    getting started with the home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  );
 }
 
 export default HomePage;
