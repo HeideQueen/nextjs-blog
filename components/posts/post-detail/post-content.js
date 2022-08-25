2675e84be97949c5df5f5291b095780e2e7b5cdf
HeideQueen <lucyfabz@gmail.com>
2022-08-25 01:37:17 -0500

    working on the post detail page

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PostHeader from './post-header';
+
+import styles from './post-content.module.css';
+
+const DUMMY_POST = {
+  slug: 'getting-started-with-nextjs',
+  title: 'Getting Started With NextJS',
+  image: 'getting-started-nextjs.png',
+  content: '# This is a first post',
+  date: '2022-08-24',
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={styles.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+}
+
+export default PostContent;
