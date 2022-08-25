7335ece87fb3bc4fa2342085c737384cbda27028
HeideQueen <lucyfabz@gmail.com>
2022-08-25 03:14:27 -0500

    using markdown data for rendering posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,43 +1,19 @@
 import React from 'react';
 import AllPosts from '../../components/posts/all-posts';
+import { getAllPosts } from '../../lib/posts-util';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React Framework for production -  it makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering',
-    date: '2022-08-24',
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React Framework for production -  it makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering',
-    date: '2022-08-24',
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React Framework for production -  it makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering',
-    date: '2022-08-24',
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React Framework for production -  it makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering',
-    date: '2022-08-24',
-  },
-];
+function AllPostsPage({ posts }) {
+  return <AllPosts posts={posts} />;
+}
 
-function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export async function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 }
 
 export default AllPostsPage;
