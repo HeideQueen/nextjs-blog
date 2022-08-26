902a197436fd22897a7b732f473cc8e3503a84ba
HeideQueen <lucyfabz@gmail.com>
2022-08-26 00:32:17 -0500

    rendering code snippets from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './post-header';
 
@@ -39,6 +41,19 @@ function PostContent({ post }) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          // eslint-disable-next-line react/no-children-prop
+          children={children}
+        />
+      );
     },
   };
 
