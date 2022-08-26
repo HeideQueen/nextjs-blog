2fd81b9b8244d6d91361e07827e31bde45ebe404
HeideQueen <lucyfabz@gmail.com>
2022-08-25 22:54:02 -0500

    rendering dymanic post pages and paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -5,21 +5,13 @@ import ReactMarkdown from 'react-markdown';
 
 import styles from './post-content.module.css';
 
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Getting Started With NextJS',
-  image: 'getting-started-nextjs.png',
-  content: '# This is a first post',
-  date: '2022-08-24',
-};
-
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent({ post }) {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={styles.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,37 @@
 import React from 'react';
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage({ post }) {
+  return <PostContent post={post} />;
+}
+
+export async function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug: slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postSlug = params.slug;
+
+  const loadedPost = getPostData(postSlug);
+
+  return {
+    props: {
+      post: loadedPost,
+    },
+    revalidate: 600,
+  };
 }
 
 export default PostDetailPage;
