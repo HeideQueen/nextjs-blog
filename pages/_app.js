20dfacfafd1e4315abea81d8bf3000b77a6697bc
HeideQueen <lucyfabz@gmail.com>
2022-09-02 03:39:52 -0500

    fixed some mongodb bugs

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <meta charset='UTF-8' />
+        <meta charSet='UTF-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -26,7 +26,7 @@ async function handler(req, res) {
 
     let client;
 
-    const connectionString = `'mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.r7fbk8y.mongodb.net/?retryWrites=true&w=majority'`;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.r7fbk8y.mongodb.net/?retryWrites=true&w=majority`;
 
     try {
       client = await MongoClient.connect(connectionString);
